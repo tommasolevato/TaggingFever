b309917c96760009ed861948d00a3a398e1200cb
tommasolevato <tommaso.levato@gmail.com>
2015-05-22 14:54:27 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,24 @@
+class Config():
+
+    dict = {}
+    with open("config") as configFile:
+            for line in configFile:
+                key = line.split("=")[0]
+                value = line.split("=")[1]
+                dict[key] = value.rstrip()
+
+    @staticmethod
+    def getUsername():
+        return Config.dict["username"]
+    
+    @staticmethod
+    def getPassword():
+        return Config.dict["password"]
+    
+    @staticmethod
+    def getHost():
+        return Config.dict["host"]
+    
+    @staticmethod
+    def getDatabase():
+        return Config.dict["database"]
---
+++ galleries.py
@@ -0,0 +1,11 @@
+import MySQLdb
+from config import Config
+
+db = MySQLdb.connect(Config.getHost(), Config.getUsername(), Config.getPassword(), Config.getDatabase())
+
+cursor = db.cursor()
+
+cursor.execute("select * from people")
+print len(cursor.fetchall())
+
+#select * from people where peopleid in (select person from probe) and cameraid="C1" and bb_x<800 and bb_y>150;

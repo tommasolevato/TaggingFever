e49445dc1cef497be5802af0877d5619fe485714
leonardogalli91 <leonardogalli2@msn.com>
2015-05-25 16:52:55 +0200

    Probe and Gallery select

--- probe_set.py
+++ probe_set.py
@@ -1,1 +1,3 @@
-#modifica inutile di prova e un'altra
+#select * from description, detection where description.detection_id=detection.id and x<753 and y>121 and h>30;
+
+#select * from description, detection where description.detection_id=detection.id and not(x<753 and y>121) and h>30;

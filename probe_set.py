7408b5df8209cc4c81b5da44265d65359abfffcc
Tommaso Levato <tommaso.levato@gmail.com>
2015-05-28 23:01:50 +0200

    Delete probe_set.py

--- probe_set.py
+++
@@ -1,3 +0,0 @@
-#select * from description, detection where description.detection_id=detection.id and x<753 and y>121 and h>30;
-
-#select * from description, detection where description.detection_id=detection.id and not(x<753 and y>121) and h>30;
